1c1e164d94fdaaef4a3b5b46219028810211422b
mTayo <m.adinlewa@gmail.com>
2025-10-03 11:42:41 +0100

    fixes

--- dist/config/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-process.on('SIGINT', async () => {
-    await prisma.$disconnect();
-    process.exit();
-});
-exports.default = prisma;
